c0b9c0ec0fe93b98527137d13b2a46f0a25681a6
Mithlesh Meghwal <115926756+mithleshmeghwal0@users.noreply.github.com>
2024-12-29 13:03:46 +0700

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/gos
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// HelloHandler handles all incoming HTTP requests
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	// Respond with "Hello, all!" to any HTTP request
+	fmt.Fprintln(w, "Hello, all!")
+}
+
+func main() {
+	// Register the HelloHandler for all HTTP requests
+	http.HandleFunc("/", HelloHandler)
+
+	// Start the server on port 8080
+	fmt.Println("Server is listening on port 8080...")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
